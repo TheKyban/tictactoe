f5a895d0f4ce68bd6f849ff36b9402c9c16473f8
Aditya <aaditya1392@gmail.com>
2023-03-06 18:47:15 +0530

    draw

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ const boxs = document.getElementsByClassName('box')
 
 Array.from(boxs).forEach((box) => {
     box.onclick = () => {
-        if (!isGame && box.innerText === '' && !checkFiled) { // checkFilled is under construction
+        if (!isGame && box.innerText === '' && !checkFiled) { 
 
             if (turn == 'X') {
                 box.style.color = "rgb(101, 145, 184)"
             } else {
                 box.style.color = "rgb(165, 154, 139)"
             }
-
             box.innerText = turn //updating box 
+
             musicPlayer("music/click.wav") // click sound
+            
             checkWin() // checking that player has won or not
+
             if (isGame) {
                 heading.innerText = "Hurray! " + turn + " Won"
             }
             checkTurn() // changing turn
-            checkFiled() // under construction
+
+            checkingFilledFull() // checking draw or not
+            if (checkFiled) { // checking draw or not
+                heading.innerText = "DRAW"
+            }
         }
     }
 })
@@ -64,12 +70,11 @@ const reset = document.querySelector('input').onclick = () => { // reset button
     heading.innerText = "Welcome Again"
 }
 
-let checkingFilledFull = () => { // under construction
+let checkingFilledFull = () => { // Draw checking
     const get = (box) => {
         return box.innerText
     }
     if (get(boxs[0]) != '' && get(boxs[1]) != '' && get(boxs[2]) != '' && get(boxs[3]) != '' && get(boxs[4]) != '' && get(boxs[5]) != '' && get(boxs[6]) != '' && get(boxs[7]) != '' && get(boxs[8]) != '') {
         checkFiled = true
-        reset()
     }
 }
